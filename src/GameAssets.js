7f84e8034fb47815d69574e0f83c0b7a7d0a3c12
Nogradi123 <nogradi.nicholas1@gmail.com>
2022-08-08 22:10:53 -0400

    cleaned up the forEach loops and started on the event listener

--- src/GameAssets.js
+++ src/GameAssets.js
@@ -4,56 +4,57 @@
 
 //^ this exactly what i was thinking too!! 
 
+
 const playerSpades = [
-     {"type":`s`, "value":1, "src":`../img/cards/Spades/ready/A.png` }
-    ,{"type":`s`, "value":2, "src":`../img/cards/Spades/ready/2.png` }
-    ,{"type":`s`, "value":3, "src":`../img/cards/Spades/ready/3.png` }
-    ,{"type":`s`, "value":4, "src":`../img/cards/Spades/ready/4.png` }
-    ,{"type":`s`, "value":5, "src":`../img/cards/Spades/ready/5.png` }
-    ,{"type":`s`, "value":6, "src":`../img/cards/Spades/ready/6.png` }
-    ,{"type":`s`, "value":7, "src":`../img/cards/Spades/ready/7.png` }
-    ,{"type":`s`, "value":8, "src":`../img/cards/Spades/ready/8.png` }
-    ,{"type":`s`, "value":9, "src":`../img/cards/Spades/ready/9.png` }
-    ,{"type":`s`, "value":10, "src":`../img/cards/Spades/ready/10.png` }
+     {type: `s`, value: 1, src: `../img/cards/Spades/ready/A.png` }
+    ,{type: `s`, value: 2, src: `../img/cards/Spades/ready/2.png` }
+    ,{type: `s`, value: 3, src: `../img/cards/Spades/ready/3.png` }
+    ,{type: `s`, value: 4, src: `../img/cards/Spades/ready/4.png` }
+    ,{type: `s`, value: 5, src: `../img/cards/Spades/ready/5.png` }
+    ,{type: `s`, value: 6, src: `../img/cards/Spades/ready/6.png` }
+    ,{type: `s`, value: 7, src: `../img/cards/Spades/ready/7.png` }
+    ,{type: `s`, value: 8, src: `../img/cards/Spades/ready/8.png` }
+    ,{type: `s`, value: 9, src: `../img/cards/Spades/ready/9.png` }
+    ,{type: `s`, value: 10, src :`../img/cards/Spades/ready/10.png`}
 ];
 
 const playerClubs = [
-    {"type":`c`, "value":1, "src":`../img/cards/Clubs/ready/A.png` }
-   ,{"type":`c`, "value":2, "src":`../img/cards/Clubs/ready/2.png` }
-   ,{"type":`c`, "value":3, "src":`../img/cards/Clubs/ready/3.png` }
-   ,{"type":`c`, "value":4, "src":`../img/cards/Clubs/ready/4.png` }
-   ,{"type":`c`, "value":5, "src":`../img/cards/Clubs/ready/5.png` }
-   ,{"type":`c`, "value":6, "src":`../img/cards/Clubs/ready/6.png` }
-   ,{"type":`c`, "value":7, "src":`../img/cards/Clubs/ready/7.png` }
-   ,{"type":`c`, "value":8, "src":`../img/cards/Clubs/ready/8.png` }
-   ,{"type":`c`, "value":9, "src":`../img/cards/Clubs/ready/9.png` }
-   ,{"type":`c`, "value":10, "src":`../img/cards/Clubs/ready/10.png` }
+    {type: `c`, value: 1, src: `../img/cards/Clubs/ready/A.png` }
+   ,{type: `c`, value: 2, src: `../img/cards/Clubs/ready/2.png` }
+   ,{type: `c`, value: 3, src: `../img/cards/Clubs/ready/3.png` }
+   ,{type: `c`, value: 4, src: `../img/cards/Clubs/ready/4.png` }
+   ,{type: `c`, value: 5, src: `../img/cards/Clubs/ready/5.png` }
+   ,{type: `c`, value: 6, src: `../img/cards/Clubs/ready/6.png` }
+   ,{type: `c`, value: 7, src: `../img/cards/Clubs/ready/7.png` }
+   ,{type: `c`, value: 8, src: `../img/cards/Clubs/ready/8.png` }
+   ,{type: `c`, value: 9, src: `../img/cards/Clubs/ready/9.png` }
+   ,{type: `c`, value: 10, src :`../img/cards/Clubs/ready/10.png` }
 ];
 
 const playerHearts = [
-    {"type":`h`, "value":1, "src":`../img/cards/Hearts/ready/A.png` }
-   ,{"type":`h`, "value":2, "src":`../img/cards/Hearts/ready/2.png` }
-   ,{"type":`h`, "value":3, "src":`../img/cards/Hearts/ready/3.png` }
-   ,{"type":`h`, "value":4, "src":`../img/cards/Hearts/ready/4.png` }
-   ,{"type":`h`, "value":5, "src":`../img/cards/Hearts/ready/5.png` }
-   ,{"type":`h`, "value":6, "src":`../img/cards/Hearts/ready/6.png` }
-   ,{"type":`h`, "value":7, "src":`../img/cards/Hearts/ready/7.png` }
-   ,{"type":`h`, "value":8, "src":`../img/cards/Hearts/ready/8.png` }
-   ,{"type":`h`, "value":9, "src":`../img/cards/Hearts/ready/9.png` }
-   ,{"type":`h`, "value":10, "src":`../img/cards/Hearts/ready/10.png` }
+    {type: `h`, value: 1, src: `../img/cards/Hearts/ready/A.png` }
+   ,{type: `h`, value: 2, src: `../img/cards/Hearts/ready/2.png` }
+   ,{type: `h`, value: 3, src: `../img/cards/Hearts/ready/3.png` }
+   ,{type: `h`, value: 4, src: `../img/cards/Hearts/ready/4.png` }
+   ,{type: `h`, value: 5, src: `../img/cards/Hearts/ready/5.png` }
+   ,{type: `h`, value: 6, src: `../img/cards/Hearts/ready/6.png` }
+   ,{type: `h`, value: 7, src: `../img/cards/Hearts/ready/7.png` }
+   ,{type: `h`, value: 8, src: `../img/cards/Hearts/ready/8.png` }
+   ,{type: `h`, value: 9, src: `../img/cards/Hearts/ready/9.png` }
+   ,{type: `h`, value: 10, src :`../img/cards/Hearts/ready/10.png` }
 ];
 
 const playerDiamonds = [
-    {"type":`d`, "value":1, "src":`../img/cards/Diamonds/ready/A.png` }
-   ,{"type":`d`, "value":2, "src":`../img/cards/Diamonds/ready/2.png` }
-   ,{"type":`d`, "value":3, "src":`../img/cards/Diamonds/ready/3.png` }
-   ,{"type":`d`, "value":4, "src":`../img/cards/Diamonds/ready/4.png` }
-   ,{"type":`d`, "value":5, "src":`../img/cards/Diamonds/ready/5.png` }
-   ,{"type":`d`, "value":6, "src":`../img/cards/Diamonds/ready/6.png` }
-   ,{"type":`d`, "value":7, "src":`../img/cards/Diamonds/ready/7.png` }
-   ,{"type":`d`, "value":8, "src":`../img/cards/Diamonds/ready/8.png` }
-   ,{"type":`d`, "value":9, "src":`../img/cards/Diamonds/ready/9.png` }
-   ,{"type":`d`, "value":10, "src":`../img/cards/Diamonds/ready/10.png` }
+    {type: `d`, value: 1, src: `../img/cards/Diamonds/ready/A.png` }
+   ,{type: `d`, value: 2, src: `../img/cards/Diamonds/ready/2.png` }
+   ,{type: `d`, value: 3, src: `../img/cards/Diamonds/ready/3.png` }
+   ,{type: `d`, value: 4, src: `../img/cards/Diamonds/ready/4.png` }
+   ,{type: `d`, value: 5, src: `../img/cards/Diamonds/ready/5.png` }
+   ,{type: `d`, value: 6, src: `../img/cards/Diamonds/ready/6.png` }
+   ,{type: `d`, value: 7, src: `../img/cards/Diamonds/ready/7.png` }
+   ,{type: `d`, value: 8, src: `../img/cards/Diamonds/ready/8.png` }
+   ,{type: `d`, value: 9, src: `../img/cards/Diamonds/ready/9.png` }
+   ,{type: `d`, value: 10, src:`../img/cards/Diamonds/ready/10.png` }
 ];
 
 const enemyJacks = [
@@ -79,15 +80,20 @@ const enemyKings = [
 
 let playerCards = [];
 
-playerSpades.forEach((card) => {
-    playerCards.push(card)
-});
-playerClubs.forEach((card) => {
-    playerCards.push(card)
-});
-playerHearts.forEach((card) => {
-    playerCards.push(card)
-});
-playerDiamonds.forEach((card) => {
-    playerCards.push(card)
-});
+function getCards(playerAllCards) {
+    playerAllCards.forEach((card) => {
+        playerCards.push(card);
+    });
+    
+};
+
+console.log(getCards(playerSpades));
+console.log(getCards(playerClubs));
+console.log(getCards(playerHearts));
+console.log(getCards(playerDiamonds));
+
+
+
+
+
+
--- src/GameCode.js
+++ src/GameCode.js
@@ -13,3 +13,16 @@ console.log(gameStart.enemyJacks)
 console.log(gameStart.enemyQueens)
 console.log(gameStart.enemyKings)
 console.log(gameStart.playerHand)
+
+
+window.addEventListener('load', (event) => {
+    let html ='';
+    gameStart.playerDeck.forEach((pic) => {
+        html += `
+        <div class = "cards" data-card-name=${pic.type}
+        <div class="front" style="background: url(${pic.src}) no-repeat></div>
+        </div>
+        `;
+    });
+})
+  
